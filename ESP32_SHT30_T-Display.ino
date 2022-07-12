9485decef3fe559e6d909b5e262687e4e60bae2d
AdamJHowell <adam.j.howell@gmail.com>
2022-07-11 23:30:47 -0600

    Added OTA updating

--- ESP32_SHT30_T-Display.ino
+++ ESP32_SHT30_T-Display.ino
@@ -15,6 +15,9 @@
 #include <PubSubClient.h>			// PubSub is the MQTT API.  Author: Nick O'Leary  https://github.com/knolleary/pubsubclient
 #include "privateInfo.h"			// I use this file to hide my network information from random people browsing my GitHub repo.
 #include <ArduinoJson.h>			// https://arduinojson.org/
+#include <ESPmDNS.h>					// OTA
+#include <WiFiUdp.h>					// OTA
+#include <ArduinoOTA.h>				// OTA
 
 #define ADC_EN					14  	// ADC_EN is the Analog to Digital Converter detection enable port.
 #define ADC_PIN				34
@@ -114,7 +117,7 @@ void onReceiveCallback( char* topic, byte* payload, unsigned int length )
 } // End of onReceiveCallback() function.
 
 
-// This function puts the ESP into shallow sleep, which saves power compared to the traditional delay().
+// This function puts the ESP32 into shallow sleep, which saves power compared to the traditional delay().
 void espDelay( int ms )
 {
 	esp_sleep_enable_timer_wakeup( ms * 1000 );
@@ -242,6 +245,56 @@ void setup()
 
 	// Try to connect to the configured WiFi network, up to 10 times.
 	wifiConnect( 10 );
+
+	// Port defaults to 3232
+	// ArduinoOTA.setPort( 3232 );
+
+	// Hostname defaults to esp32-[MAC]
+	// ArduinoOTA.setHostname( "myesp32" );
+
+	// No authentication by default
+	// ArduinoOTA.setPassword( "admin" );
+
+	// Password can be set with it's md5 value as well
+	// MD5( admin ) = 21232f297a57a5a743894a0e4a801fc3
+	// ArduinoOTA.setPasswordHash( "21232f297a57a5a743894a0e4a801fc3" );
+
+	ArduinoOTA
+		.onStart( []()
+		{
+			String type;
+			if( ArduinoOTA.getCommand() == U_FLASH )
+				type = "sketch";
+			else // U_SPIFFS
+				type = "filesystem";
+
+			// NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+			Serial.println( "Start updating " + type );
+		} )
+		.onEnd( []()
+		{
+			Serial.println( "\nEnd" );
+		} )
+		.onProgress( []( unsigned int progress, unsigned int total )
+		{
+			Serial.printf( "Progress: %u%%\r", ( progress / ( total / 100 ) ) );
+		} )
+		.onError( []( ota_error_t error )
+		{
+			Serial.printf( "Error[%u]: ", error );
+			if( error == OTA_AUTH_ERROR )
+				Serial.println( "Auth Failed" );
+			else if( error == OTA_BEGIN_ERROR )
+				Serial.println( "Begin Failed" );
+			else if( error == OTA_CONNECT_ERROR )
+				Serial.println( "Connect Failed" );
+			else if( error == OTA_RECEIVE_ERROR )
+				Serial.println( "Receive Failed" );
+			else if( error == OTA_END_ERROR )
+				Serial.println( "End Failed" );
+		} );
+
+	ArduinoOTA.begin();
 
 	// Print the current values.
 	result = sht3xd.periodicFetchData();
